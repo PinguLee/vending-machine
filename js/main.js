82265e6c21d2816ecb750c755bd8857413d4814d
leemingoo <dwmapi@naver.com>
2023-09-27 18:08:20 +0900

    err modify

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const drawButton = document.getElementById("draw");
 const total = document.getElementById("total");
 const priceText = document.getElementById("price-text");
 const admin = document.getElementById("admin");
+const adminBox = document.getElementById("admin-box")
 const addFood = document.getElementById("add-food");
 const addName = document.getElementById("add-name");
 const addPrice = document.getElementById("add-price");
@@ -18,7 +19,7 @@ let sum = 0;
 let temp = [];
 
 
-function createItemFunc() {
+function createItemFunc(foodA, nameA, priceA) {
   let createItem = document.createElement("li");
   let createLabel1 = document.createElement("label");
   let createLabel2 = document.createElement("label");
@@ -29,14 +30,14 @@ function createItemFunc() {
   createItem.className = "item";
 
   createItem.appendChild(createLabel1);
-  createLabel1.textContent = addFood.value;
+  createLabel1.textContent = foodA;
 
 
   createItem.appendChild(createLabel2);
-  createLabel2.textContent = addName.value;
+  createLabel2.textContent = nameA;
 
   createItem.appendChild(createLabel3);
-  createLabel3.textContent = addPrice.value;
+  createLabel3.textContent = priceA;
 
   createItem.appendChild(createLabel4);
   for (let i = 0; i < choiceBox.childElementCount; i++) {
@@ -55,6 +56,7 @@ function deleteItemFunc(num) {
 
 item.forEach(function (box) {
   box.addEventListener("click", function (e) {
+    
     if (checked[box.children[3].textContent - 1] !== 1) {
       checked[box.children[3].textContent - 1] = 1;
       box.style.backgroundColor = "pink";
@@ -70,14 +72,14 @@ item.forEach(function (box) {
 });
 
 drawButton.addEventListener("click", function (e) {
-  if (sum <= priceText.value && priceText.value !== "") {
-    for (let i = 0; i < item.length; i++) {
+  let price = priceText.value;
+  if (sum <= price && price !== "" && sum !== 0) {
+    for (let i = 0; i < choiceBox.childElementCount; i++) {
       if (checked[i] === 1) {
-        temp += item[i].children[0].textContent;
+        temp += liArr[i].children[0].textContent;
       }
     }
-    console.log(`${temp}을(를) 획득`);
-
+    console.log(`${temp} 획득`);
     if (sum === priceText.value) {
       console.log("잔돈 : 없음");
       priceText.textContent = "";
@@ -86,20 +88,23 @@ drawButton.addEventListener("click", function (e) {
       console.log(`거스름돈 : ${priceText.value - sum}원`);
     }
   } else {
-    console.log(`금액 부족 : ${sum - priceText.value}원`);
+    if (sum !== 0) {
+      console.log(`금액 부족 : ${sum - priceText.value}원`);
+    } 
   }
+
   temp = "";
 });
 
 admin.addEventListener("click", function (e) {
   let password = prompt("관리자 비밀번호");
   if (password === "asd") {
-
+    adminBox.style.cssText = `display: flex; `;
   }
 });
 
 add.addEventListener("click", function (e) {
-  createItemFunc();
+  createItemFunc(addFood.value, addName.value, addPrice.value);
 });
 
 del.addEventListener("click", function (e) {
